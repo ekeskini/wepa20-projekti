f7d9daf54d1a5a65dc66a62e9c130bdd2bf9f01a
ekeskini <elias@keski-nisula.fi>
2020-06-18 16:44:26 +0300

    more fixes

--- src/main/java/wepa20/repositories/AccountRepository.java
+++ src/main/java/wepa20/repositories/AccountRepository.java
@@ -1,8 +1,10 @@
 package wepa20.repositories;
 
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wepa20.entities.Account;
 
+@Transactional
 public interface AccountRepository extends JpaRepository<Account, Long>{
     Account findByUsername(String username);
 }
